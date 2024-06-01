078e0bed02e1f1aa313ce0605a19fbf57225e80f
Adam Kołodziejek <adam_kolodziejek@wp.pl>
2024-06-01 17:38:28 +0200

    update

--- read_from_pico.py
+++ read_from_pico.py
@@ -3,10 +3,10 @@ import os
 
 while True:
     os.system("cd /home/pi/Desktop/repo/rpi/ && gnuplot -e \"datafile='baobab.csv'; load 'settings_baobab.gp'\" plot.gp")
-    sleep(60)
+    sleep(15)
     os.system("cd /home/pi/Desktop/repo/rpi/ && git add .")
-    sleep(60)
+    sleep(15)
     os.system("cd /home/pi/Desktop/repo/rpi/ && git commit -m \"update\"")
-    sleep(60)
+    sleep(15)
     os.system("cd /home/pi/Desktop/repo/rpi/ && git push")
-    sleep(60)
+    sleep(15)
